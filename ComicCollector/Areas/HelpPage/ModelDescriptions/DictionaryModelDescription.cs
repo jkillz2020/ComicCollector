0a994417b8fc843ce3d9f364a668b3b4e906d84f
Josh Kilgore <kilgore.josh@gmail.com>
2017-06-05 11:02:08 -0500

    initial project commit

---
+++ ComicCollector/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ComicCollector.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ComicCollector/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ComicCollector.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
